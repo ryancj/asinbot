b86fd568320e4c1f3cf97148d4508fb4a11499de
Ryan Kayu Ching <ryankching29@gmail.com>
2018-01-28 00:29:03 -0800

    Add frontend to git

---
+++ asinbot-front/dev/js/actions/index.js
@@ -0,0 +1,4 @@
+import {GET_PRODUCTS} from './types';
+import axios from 'axios';
+
+const API_URL = "http://localhost:5000"
---
+++ asinbot-front/dev/js/components/ProductsIndex.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+export default class ProductsIndex extends Component {
+  render(){
+    return (
+      <div className="container">
+        Products Index
+      </div>
+    )
+  }
+}
