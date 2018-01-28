249ffd6732f0b802abf138718f906d73f39a7bd2
Ryan Kayu Ching <ryankching29@gmail.com>
2018-01-28 00:41:30 -0800

    Setup API controller

--- asinbot-api/app/controllers/api/v1/products_controller.rb
+++ asinbot-api/app/controllers/api/v1/products_controller.rb
@@ -1,8 +1,10 @@
 module Api
   module V1
     class ProductsController < ApplicationController
+      before_action :set_product, only: [:show, :destroy]
 
       def index
+        render json: Products.all
       end
 
       def new
@@ -26,6 +28,11 @@ module Api
       private
 
       def set_product
+        @product = Product.find(params[:id])
+      end
+
+      def product_params
+        params.permit(:product_name, :avg_rating, :total_reviews, :asin)
       end
 
     end
